e80be91b0a70985b0da588496d5af108bb1dfe06
Andrey Sergeev <andreyinoks93@yandex.ru>
2020-06-05 22:30:06 +0700

    Второе задание выполнено

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,10 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = 'Разработка сайтов';
+let addExpenses = 'ЖКХ, кредит, интернет, продукты';
+let deposit = true;
+let mission = 1000000;
+let period = 4;
 
-alert('Это сообщение появится во всплывающем окне');
-
-console.log('Это сообщение появится в консоле');
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' +  period + ' месяцев');
+console.log('Цель заработать ' +  mission + ' рублей');
+console.log(addExpenses.toLocaleLowerCase().split(', '));
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
