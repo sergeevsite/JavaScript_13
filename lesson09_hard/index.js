53c72671eb13d86ccd753c99435f159b85bd2e0f
Andrey Sergeev <sergeev_a@joy.money>
2020-06-17 17:35:47 +0700

    Обязательно и усложненное задание 9

--- lesson09_hard/index.js
+++ lesson09_hard/index.js
@@ -1,24 +1,39 @@
 'use strict';
 
-const outputTime = document.querySelectorAll('.time');
+const fieldDate = document.querySelectorAll('.time');
 
-let date = new Date(),
-    weekDaysWord = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    monthDaysWord = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Ноябрь', 'Декабрь'],
-    weekDays = date.getDay(),
-    day = date.getDate(),
-    month = date.getMonth(),
-    year = date.getFullYear(),
-    hours = date.getHours(),
-    minutes = date.getMinutes(),
-    seconds = date.getSeconds();
+// Функция добавляет 0 перед значениями, которые состоят из одной цифры
+function convertClock(number) {
+  return number = (number < 10) ? '0' + number : number;
+}
+// Функция преобразует слова в зависимости от числа
+function changeDeclensionWords(number, word) {
+  let n1 = number % 10;
+  if (number === 11 || n1 === 0 || n1 >= 5 || number >= 12 && number <= 19) return word[2];
+  if (n1 === 1) return word[0];
+  if (n1 > 1 && n1 < 5) return word[1];
+}
 
-let timeFormat1 = 'Сегодня ' + weekDaysWord[weekDays] + ', ' + day + ' ' + monthDaysWord[month] + ' ' + year + ' года, ' + hours + ' час ' + minutes + ' минут ' + seconds + ' секунды';
-let timeFormat2 = 1;
+function outputDate() {
+  // Переменные даты
+  let date = new Date(),
+      weekDaysWord = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      monthDaysWord = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Ноября', 'Декабря'],
+      weekDays = date.getDay(),
+      day = date.getDate(),
+      month = date.getMonth(),
+      year = date.getFullYear(),
+      hours = date.getHours(),
+      minutes = date.getMinutes(),
+      seconds = date.getSeconds();
 
-function changeDeclensionWords() {
+// Вывод первого формата
+fieldDate[0].textContent = 'Сегодня ' + weekDaysWord[weekDays] + ', ' + day + ' ' + monthDaysWord[month] + ' ' + year + ' года, ' + hours + changeDeclensionWords(hours, [' час', ' часа', ' часов']) + ' ' + minutes + changeDeclensionWords(minutes, [' минута', ' минуты', ' минут']) + ' ' + seconds + changeDeclensionWords(seconds, [' секунда', ' секунды', ' секунд']);
+
+// Вывод второго формата
+fieldDate[1].textContent = convertClock(day) + '.' + convertClock(month) + '.' + year + ' - ' + convertClock(hours) + ':' + convertClock(minutes) + ':' + convertClock(seconds);
 
 }
 
-outputTime[0].textContent = timeFormat1;
-outputTime[1].textContent = date.toLocaleDateString() + ' - ' + date.toLocaleTimeString();
+outputDate();
+setInterval(outputDate, 1000);
