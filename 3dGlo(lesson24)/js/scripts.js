916e3b38237adce62178ffd3348d54fca75f8a33
Andrey Sergeev <andreyinoks93@yandex.ru>
2020-07-08 08:47:46 +0700

    lesson24 done

--- 3dGlo(lesson24)/js/scripts.js
+++ 3dGlo(lesson24)/js/scripts.js
@@ -321,4 +321,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
   fieldNumber('.calc-item');
 
+  // Калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcCount = document.querySelector('.calc-count'),
+          calcDay = document.querySelector('.calc-day'),
+          totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+          countValue = 1,
+          dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+      if(calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10
+      }
+
+      if(calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if(calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if(typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      let target = event.target;
+
+      if(target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
+  calc(100);
 });
