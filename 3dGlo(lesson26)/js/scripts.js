aacae4e41c8a13a0256f751910c34296c00fc937
Andrey Sergeev <andreyinoks93@yandex.ru>
2020-07-12 09:54:36 +0700

    lesson26 done

--- 3dGlo(lesson26)/js/scripts.js
+++ 3dGlo(lesson26)/js/scripts.js
@@ -309,17 +309,70 @@ window.addEventListener('DOMContentLoaded', () => {
 
   getChangeImage();
 
-  // Ввод только цифр
-  const fieldNumber = (selector) => {
-    const fields = document.querySelectorAll(selector);
-    fields.forEach((field) => {
-      field.addEventListener('input', () => {
-        field.value = field.value.replace(/\D/, '');
-      });
-    });
-  };
+  // Валидация полей
 
-  fieldNumber('input.calc-item');
+    // Ввод только цифр
+    const fieldNumber = (wrapper, selector) => {
+
+      if(selector !== undefined) {
+        selector = `input[name="${selector}"]`;
+      } else {
+        selector = 'input'
+      }
+
+      const form = document.querySelector(wrapper),
+            fields = form.querySelectorAll(selector);
+
+      fields.forEach( field => {
+        field.addEventListener('input', () => {
+          field.value = field.value.replace(/\D/, '');
+        });
+      });
+    };
+
+    // Валидация при отправке
+    const fieldValidate = (idForm, attrName, type) => {
+
+      const form = document.getElementById(idForm),
+            field = form.querySelector( `input[name="${attrName}"]`);
+
+      // Подстановка паттерна в зависимости от типа поля
+      let pattern;
+      if(type === 'phone') {
+        pattern = /^\+?[78]([-()]*\d){10}$/ig;
+      } else if(type === 'email') {
+        pattern = /^\w+@\w+\.\w{2,}$/ig;
+      }
+
+      if(field.value.match(pattern) === null) {
+        field.style.cssText = 'background-color: #ffcccc;';
+        return field.value.match(pattern);
+      } else {
+        field.style.cssText = 'background-color: #fff;';
+        return field.value.match(pattern);
+      }
+      
+    };
+
+   // Валидация при вводе
+    const fieldValidateChange = (idForm, attrName, type) => {
+
+      const form = document.getElementById(idForm),
+            field = form.querySelector( `input[name="${attrName}"]`);
+
+      // Подстановка паттерна в зависимости от типа поля
+      let patternChange;
+      if(type === 'phone') {
+        patternChange = /[^\+\d]/;
+      } else if(type === 'text_ru') {
+        patternChange = /^\s?[^А-ЯЁа-яё]/ig;
+      }
+      
+      field.addEventListener('input', () => {
+        field.value = field.value.replace(patternChange, '');
+      });
+
+    };
 
   // Калькулятор
   const calc = (price = 100) => {
@@ -346,14 +399,32 @@ window.addEventListener('DOMContentLoaded', () => {
       } else if(calcDay.value && calcDay.value < 10) {
         dayValue *= 1.5;
       }
-
+      
+      
       if(typeValue && squareValue) {
         total = price * typeValue * squareValue * countValue * dayValue;
       }
+      
+      let start = +totalValue.textContent;
+      let end = total;
 
-      totalValue.textContent = total;
+      const animateCount = setInterval(() => {
+            if(start > end) {
+              total = start--;
+              if(start < end) {
+                clearInterval(animateCount);
+              }
+            } else {
+              total = start++;
+              if (start > end) {
+              clearInterval(animateCount);
+              }
+            }
+            totalValue.textContent = total;
+
+          }, 5);
     };
-
+    
     calcBlock.addEventListener('change', (event) => {
       let target = event.target;
 
@@ -362,6 +433,100 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
   };
-
+  
+  fieldNumber('.calc-block');
   calc(100);
+
+  // send-ajax-form
+  const sendForm = (idForm) => {
+    const erorrMessage = 'Что то пошло не так ...',
+          loadMessage = 'Загрузка...',
+          successMessage = 'Спасибо! Мы скоро свяжемся с вами!',
+          validateMessage = 'Данные введены не верно';
+
+    const form = document.getElementById(idForm);
+
+    fieldValidateChange(idForm, 'user_phone', 'phone');
+    fieldValidateChange(idForm, 'user_email', 'email');
+    fieldValidateChange(idForm, 'user_name', 'text_ru');
+    fieldValidateChange('form2', 'user_message', 'text_ru');
+
+    // Создаем элемент с текстом статуса
+    const statusMessages = document.createElement('span');
+    statusMessages.style.cssText = 'display: block; font-size: 14px;margin: 8px 0; color: white;';
+    
+    // Вешаем событие отправки
+    form.addEventListener('submit', (event) => {
+
+      event.preventDefault();
+      form.appendChild(statusMessages);
+      statusMessages.textContent = loadMessage;
+
+      // Формируем данные к отправке
+      const formData = new FormData(form);
+      let body = {};
+
+      formData.forEach((value, index) => {
+        body[index] = value;
+      });
+
+      // Валидация
+      const validPhone = fieldValidate(idForm, 'user_phone', 'phone'),
+            validEmail = fieldValidate(idForm, 'user_email', 'email'),
+            validName = fieldValidate(idForm, 'user_name', 'text_ru'),
+            validMessage = fieldValidate('form2', 'user_message', 'text_ru');
+
+      if(validPhone !== null && validEmail !== null && validName !== null && validMessage !== null) {
+        // Вызываем отправку данных
+        postData(body, 
+        () => {
+          statusMessages.textContent = successMessage;
+          // Очистка полей
+          form.querySelectorAll('input').forEach( item => {
+            item.value = '';
+          });
+        }, 
+        () => {
+          statusMessages.textContent = erorrMessage;
+        });
+      } else {
+        statusMessages.textContent = validateMessage;
+      }
+
+      
+    });
+  };
+
+  const postData = (body, outputData, errorData) => {
+      
+    // Создание запроса
+    const request = new XMLHttpRequest();
+
+    // Событие для отслеживания статуса
+    request.addEventListener('readystatechange', () => {
+
+      if(request.readyState !== 4) {
+        return;
+      }
+
+      if(request.status === 200) {
+        outputData();
+      } else {
+        errorData(request.status);
+      }
+
+    });
+
+    // Отправка данных через сервер
+    request.open('POST', './server.php');
+    request.setRequestHeader('Content-Type', 'application/json');
+    
+    // Преобразование в JSON
+    request.send(JSON.stringify(body));
+  }
+
+  sendForm('form1');
+  sendForm('form2');
+  sendForm('form3');
+
 });
